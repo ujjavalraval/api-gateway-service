fd4bdf491b7d0b77e62dc3a1b01c00dd56e62803
ujjavalraval <ujjaval.raval@neebal.com>
2023-12-13 15:45:50 +0530

    added code in develop

---
+++ src/main/java/app/gateway/GatewayServiceApplication.java
@@ -0,0 +1,17 @@
+package app.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import io.netty.resolver.DefaultAddressResolverGroup;
+import reactor.netty.http.client.HttpClient;
+
+@SpringBootApplication
+public class GatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+	
+}
